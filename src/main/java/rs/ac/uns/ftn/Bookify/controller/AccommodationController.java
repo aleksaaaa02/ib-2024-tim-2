d613fcd1142073721836f9b3e7d951de3dc83d3c
Olivera2708 <olivera.radovanovic2002@gmail.com>
2023-11-22 00:02:49 +0100

    added new mappings for favorites

--- src/main/java/rs/ac/uns/ftn/Bookify/controller/AccommodationController.java
+++ src/main/java/rs/ac/uns/ftn/Bookify/controller/AccommodationController.java
@@ -1,7 +1,7 @@
 package rs.ac.uns.ftn.Bookify.controller;
 
-import org.apache.tomcat.util.http.parser.MediaType;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import rs.ac.uns.ftn.Bookify.dto.AccommodationBasicDTO;
@@ -12,7 +12,7 @@ import java.util.Collection;
 import java.util.HashSet;
 
 @RestController
-@RequestMapping("/api/v1/reservation")
+@RequestMapping("/api/v1/accommodation")
 public class AccommodationController {
     //    @Autowired
     //    private IAccommodationService accommodationService;
@@ -38,11 +38,24 @@ public class AccommodationController {
         return new ResponseEntity<>(basicAccommodations, HttpStatus.OK);
     }
 
+    @GetMapping("/favorites/{guestId}")
+    public ResponseEntity<Collection<AccommodationBasicDTO>> getFavoritesAccommodations(@PathVariable Long guestId){
+        //returns all favorites accommodation of user
+        Collection<AccommodationBasicDTO> basicAccommodations = new HashSet<>();
+        return new ResponseEntity<>(basicAccommodations, HttpStatus.OK);
+    }
+
     @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
     public ResponseEntity<AccommodationDTO> insert(@RequestBody AccommodationDTO accommodation) {
         //insert new accommodation
-        AccommodationDTO savedReservation = new AccommodationDTO();
-        return new ResponseEntity<AccommodationDTO>(savedReservation, HttpStatus.CREATED);
+        AccommodationDTO savedAccommodation = new AccommodationDTO();
+        return new ResponseEntity<AccommodationDTO>(savedAccommodation, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/add-to-favorites/{guestId}/{accommodationId}")
+    public ResponseEntity<String> addAccommodationToFavorites(@PathVariable Long guestId, @PathVariable Long accommodationId){
+        //inserts accommodation to favorites
+        return new ResponseEntity<>("Accommodation added to favorites", HttpStatus.OK);
     }
 
     @PutMapping(value = "/{accommodationId}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
@@ -50,6 +63,12 @@ public class AccommodationController {
         //update accommodation
         AccommodationDTO accommodationDTO = new AccommodationDTO();
         return new ResponseEntity<AccommodationDTO>(accommodationDTO, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/remove-from-favorites/{guestId}/{accommodationId}")
+    public ResponseEntity<String> removeAccommodationFromFavorites(@PathVariable Long guestId, @PathVariable Long accommodationId){
+        //delete accommodation from favorites
+        return new ResponseEntity<>("Accommodation removed from favorites", HttpStatus.OK);
     }
 
     @DeleteMapping("/{accommodationId}")
