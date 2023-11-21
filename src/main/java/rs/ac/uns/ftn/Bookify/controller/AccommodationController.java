601592adcfe8e7ab654df20a0b61a1a5a8def5a0
Olivera2708 <olivera.radovanovic2002@gmail.com>
2023-11-21 12:56:15 +0100

    dto and controller

--- src/main/java/rs/ac/uns/ftn/Bookify/controller/AccommodationController.java
+++ src/main/java/rs/ac/uns/ftn/Bookify/controller/AccommodationController.java
@@ -1,4 +1,60 @@
 package rs.ac.uns.ftn.Bookify.controller;
 
+import org.apache.tomcat.util.http.parser.MediaType;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import rs.ac.uns.ftn.Bookify.dto.AccommodationBasicDTO;
+import rs.ac.uns.ftn.Bookify.dto.AccommodationDTO;
+import rs.ac.uns.ftn.Bookify.dto.AccommodationDetailDTO;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+@RestController
+@RequestMapping("/api/v1/reservation")
 public class AccommodationController {
+    //    @Autowired
+    //    private IAccommodationService accommodationService;
+
+    @GetMapping(produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Collection<AccommodationBasicDTO>> getAccommodationBasics(){
+        //return all basic info of accommodations
+        Collection<AccommodationBasicDTO> basicAccommodations = new HashSet<>();
+        return new ResponseEntity<>(basicAccommodations, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{accommodationId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<AccommodationDetailDTO> getAccommodationDetails(@PathVariable Long accommodationId){
+        //returns details about one accommodation
+        AccommodationDetailDTO accommodationDetailDTO = new AccommodationDetailDTO();
+        return new ResponseEntity<>(accommodationDetailDTO, HttpStatus.OK);
+    }
+
+    @GetMapping(value = "/{ownerId}", produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<Collection<AccommodationBasicDTO>> getOwnersAccommodations(@PathVariable Long ownerId){
+        //returns all accommodations for owner
+        Collection<AccommodationBasicDTO> basicAccommodations = new HashSet<>();
+        return new ResponseEntity<>(basicAccommodations, HttpStatus.OK);
+    }
+
+    @PostMapping(consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<AccommodationDTO> insert(@RequestBody AccommodationDTO accommodation) {
+        //insert new accommodation
+        AccommodationDTO savedReservation = new AccommodationDTO();
+        return new ResponseEntity<AccommodationDTO>(savedReservation, HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "/{accommodationId}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<AccommodationDTO> updateAccommodation(@PathVariable Long accommodationId) {
+        //update accommodation
+        AccommodationDTO accommodationDTO = new AccommodationDTO();
+        return new ResponseEntity<AccommodationDTO>(accommodationDTO, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/{accommodationId}")
+    public ResponseEntity<AccommodationDTO> delete(@PathVariable Long accommodationId) {
+        //delete accommodation
+        return new ResponseEntity<AccommodationDTO>(HttpStatus.NO_CONTENT);
+    }
 }
--- src/main/java/rs/ac/uns/ftn/Bookify/dto/AccommodationBasicDTO.java
+++ src/main/java/rs/ac/uns/ftn/Bookify/dto/AccommodationBasicDTO.java
@@ -1,12 +1,18 @@
 package rs.ac.uns.ftn.Bookify.dto;
 
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
+import rs.ac.uns.ftn.Bookify.enumerations.PricePer;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class AccommodationBasicDTO {
-    //name, address, avg rating, total price, price per, price for price per
+    private Long id;
+    private String name;
+    private String address;
+    private float avgRating;
+    private float totalPrice;
+    public PricePer pricePer;
+    public float priceOne;
 }
+
