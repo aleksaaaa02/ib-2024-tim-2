71fcc4f5cb3761ac01440bf8ba216d9158a0bbc9
Milos Bojanic <mbojanic53@gmail.com>
2023-11-22 16:34:48 +0100

    Added class and methods for reporting users and dummy objects

--- src/main/java/rs/ac/uns/ftn/Bookify/controller/UserController.java
+++ src/main/java/rs/ac/uns/ftn/Bookify/controller/UserController.java
@@ -1,4 +1,39 @@
 package rs.ac.uns.ftn.Bookify.controller;
 
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import rs.ac.uns.ftn.Bookify.dto.ReportedUserDTO;
+import rs.ac.uns.ftn.Bookify.model.Guest;
+import rs.ac.uns.ftn.Bookify.model.Owner;
+
+import java.time.LocalDateTime;
+
+@RestController
+@RequestMapping("/api/v1/user")
 public class UserController {
+
+    @PostMapping(value = "/report", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ReportedUserDTO> insertReport(@RequestBody ReportedUserDTO reservation) {
+        //insert new report
+        ReportedUserDTO reportedUserDTO = new ReportedUserDTO("Reason", LocalDateTime.of(2000,10,10,10,10,10), new Owner(), new Guest());
+        return new ResponseEntity<ReportedUserDTO>(reportedUserDTO, HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "/block/{userId}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ReportedUserDTO> blockUser(@PathVariable Long userId) {
+        //change user status into blocked
+        ReportedUserDTO reportedUserDTO = new ReportedUserDTO("Reason", LocalDateTime.of(2000,10,10,10,10,10), new Owner(), new Guest());
+        return new ResponseEntity<ReportedUserDTO>(reportedUserDTO, HttpStatus.OK);
+    }
+
+    @PutMapping(value = "/unblock/{userId}", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<ReportedUserDTO> unblockUser(@PathVariable Long userId) {
+        //change user status
+        ReportedUserDTO reportedUserDTO = new ReportedUserDTO("Reason", LocalDateTime.of(2000,10,10,10,10,10), new Owner(), new Guest());
+        return new ResponseEntity<ReportedUserDTO>(reportedUserDTO, HttpStatus.OK);
+    }
 }
+
--- src/main/java/rs/ac/uns/ftn/Bookify/dto/ReportedUserDTO.java
+++ src/main/java/rs/ac/uns/ftn/Bookify/dto/ReportedUserDTO.java
@@ -3,9 +3,20 @@ package rs.ac.uns.ftn.Bookify.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+import rs.ac.uns.ftn.Bookify.model.User;
+
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 public class ReportedUserDTO {
+
+    private String reason;
+    private LocalDateTime created;
+    private User reportedUser;
+    private User createdBy;
 }
+
+
+
