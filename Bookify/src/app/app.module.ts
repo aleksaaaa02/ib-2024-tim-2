ee2d0b64933ef6a02089bf620895f2be930cea22
Olivera2708 <olivera.radovanovic2002@gmail.com>
2023-11-25 21:35:03 +0100

    search, filter, accommodation-basic just html some css

--- Bookify/src/app/app.module.ts
+++ Bookify/src/app/app.module.ts
@@ -3,14 +3,34 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AccommodationBasicComponent } from './feature-modules/accommodation-basic/accommodation-basic.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatButtonToggleModule} from "@angular/material/button-toggle";
+import {MatCardModule} from "@angular/material/card";
+import {MatButtonModule} from "@angular/material/button";
+import { FilterComponent } from './feature-modules/filter/filter.component';
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatSliderModule} from "@angular/material/slider";
+import {MatInputModule} from "@angular/material/input";
+import { SearchComponent } from './feature-modules/search/search.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AccommodationBasicComponent,
+    FilterComponent,
+    SearchComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    MatButtonToggleModule,
+    MatCardModule,
+    MatButtonModule,
+    MatCheckboxModule,
+    MatSliderModule,
+    MatInputModule
   ],
   providers: [
     provideClientHydration()
