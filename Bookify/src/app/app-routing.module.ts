b65d8351cc65bd0f3f9891330b8dea1c436276ec
Olivera2708 <olivera.radovanovic2002@gmail.com>
2023-11-28 17:40:34 +0100

    added routing

--- Bookify/src/app/app-routing.module.ts
+++ Bookify/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {ResultsPageComponent} from "./feature-modules/results-page/results-page.component";
+import {AccommodationPageComponent} from "./feature-modules/accommodation-page/accommodation-page.component";
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: "results", component: ResultsPageComponent },
+  { path: "accommodation", component: AccommodationPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
