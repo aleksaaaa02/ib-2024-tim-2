0b40c174c463c96f027abb3327186b5145cfb208
Olivera2708 <olivera.radovanovic2002@gmail.com>
2023-11-29 12:26:37 +0100

    merge

--- Bookify/src/app/app.module.ts
+++ Bookify/src/app/app.module.ts
@@ -3,27 +3,27 @@ import { BrowserModule, provideClientHydration } from '@angular/platform-browser
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { AccommodationBasicComponent } from './feature-modules/accommodation-basic/accommodation-basic.component';
+import { AccommodationBasicComponent } from './feature-modules/accommodation/accommodation-basic/accommodation-basic.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import {MatButtonToggleModule} from "@angular/material/button-toggle";
 import {MatCardModule} from "@angular/material/card";
 import {MatButtonModule} from "@angular/material/button";
-import { FilterComponent } from './feature-modules/filter/filter.component';
+import { FilterComponent } from './layout/filter/filter.component';
 import {MatCheckboxModule} from "@angular/material/checkbox";
 import {MatSliderModule} from "@angular/material/slider";
 import {MatInputModule} from "@angular/material/input";
-import { SearchComponent } from './feature-modules/search/search.component';
-import { ResultsPageComponent } from './feature-modules/results-page/results-page.component';
+import { SearchComponent } from './layout/search/search.component';
+import { ResultsPageComponent } from './feature-modules/accommodation/results-page/results-page.component';
 import {MatSelectModule} from "@angular/material/select";
 import {MatPaginatorModule} from "@angular/material/paginator";
 import {MatDatepickerModule} from "@angular/material/datepicker";
-import { AccommodationPageComponent } from './feature-modules/accommodation-page/accommodation-page.component';
+import { AccommodationPageComponent } from './feature-modules/accommodation/accommodation-page/accommodation-page.component';
 import {NgImageSliderModule} from "ng-image-slider";
-import { ReserveComponent } from './feature-modules/reserve/reserve.component';
-import { LandingPageComponent } from './feature-modules/landing-page/landing-page.component';
-import { TopDestinationComponent } from './feature-modules/top-destination/top-destination.component';
-import { TopAccommodationComponent } from './feature-modules/top-accommodation/top-accommodation.component';
-import { FooterComponent } from './feature-modules/footer/footer.component';
+import { ReserveComponent } from './feature-modules/accommodation/reserve/reserve.component';
+import { LandingPageComponent } from './layout/landing-page/landing-page.component';
+import { TopDestinationComponent } from './layout/top-destination/top-destination.component';
+import { TopAccommodationComponent } from './layout/top-accommodation/top-accommodation.component';
+import { FooterComponent } from './layout/footer/footer.component';
 import { AuthenticationModule } from './feature-modules/authentication/authentication.module';
 import { MatFormFieldModule } from '@angular/material/form-field'
 
