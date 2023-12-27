1c60fee1b25ff8354f0e5be5173679df28ec24ff
Milos Bojanic <mbojanic53@gmail.com>
2023-12-27 01:33:23 +0100

    Add getting rating for owner

--- Bookify/src/app/feature-modules/review/ratings/ratings.component.ts
+++ Bookify/src/app/feature-modules/review/ratings/ratings.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ReviewService } from '../review.service';
+import { ActivatedRoute } from '@angular/router';
+import { RatingDTO } from '../model/rating.model.dto';
 
 @Component({
   selector: 'app-ratings',
   templateUrl: './ratings.component.html',
   styleUrl: './ratings.component.css'
 })
-export class RatingsComponent {
+export class RatingsComponent implements OnInit {
+  ownerId: number;
 
+  oneStar: number;
+  twoStars: number;
+  threeStars: number;
+  fourStars: number;
+  fiveStars: number;
+  sum: number;
+
+  sumProgress: number;
+
+  constructor(private reviewServise: ReviewService, private route: ActivatedRoute) { }
+
+  ngOnInit(): void {
+    this.route.params.subscribe(params => {
+      this.ownerId = +params['ownerId'];
+    });
+    if (!Number.isNaN(this.ownerId)) {
+      this.reviewServise.getOwnerRating(this.ownerId).subscribe({
+        next: (rating: RatingDTO) => {
+          this.oneStar = rating.oneStars;
+          this.twoStars = rating.twoStars;
+          this.threeStars = rating.threeStars;
+          this.fourStars = rating.fourStars;
+          this.fiveStars = rating.fiveStars;
+          this.sum = this.oneStar + this.twoStars + this.threeStars + this.fourStars + this.fiveStars;
+
+          const temp = this.oneStar + 2 * this.twoStars + 3 * this.threeStars + 4 * this.fourStars + 5 * this.fiveStars;
+          this.sumProgress = temp / this.sum;
+          console.log(this.sumProgress);
+        }
+      })
+    }
+  }
 }
--- Bookify/src/app/feature-modules/review/review.service.ts
+++ Bookify/src/app/feature-modules/review/review.service.ts
@@ -3,6 +3,7 @@ import { Inject, Injectable, LOCALE_ID } from '@angular/core';
 import { Observable } from 'rxjs';
 import { OwnerDTO } from './model/owner.model.dto';
 import { environment } from '../../../env/env';
+import { RatingDTO } from './model/rating.model.dto';
 
 @Injectable({
   providedIn: 'root'
@@ -13,5 +14,9 @@ export class ReviewService {
 
   getOwnerDTO(ownerId: number): Observable<OwnerDTO>{
     return this.httpClient.get<OwnerDTO>(environment.apiUser + "/owner/" + ownerId);
+  }
+
+  getOwnerRating(ownerId: number): Observable<RatingDTO>{
+    return this.httpClient.get<RatingDTO>(environment.apiReview + "/owner/" + ownerId + "/rating");
   }
 }
